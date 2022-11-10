7158e752acba1f135cee5a26f0df2124b24d65cc
ReeceNana <93201294+ReeceNana@users.noreply.github.com>
2022-11-10 14:17:15 +0000

    Client service (#11)
    * first-draft
    
    * first-draft
    
    * Update to finish/pom.xml
    
    * Update to finish/client/src/main/server.xml
    
    * update to contributing.md and finish/SystemClient.java

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -14,10 +14,10 @@ mvn -Dhttp.keepAlive=false \
 mvn -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
-    -ntp -pl frontend -q clean package liberty:create liberty:install-feature liberty:deploy
+    -ntp -pl client -q clean package liberty:create liberty:install-feature liberty:deploy
 
 mvn -ntp -pl system liberty:start
-mvn -ntp -pl frontend liberty:start
+mvn -ntp -pl client liberty:start
 
 mvn -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
@@ -25,10 +25,10 @@ mvn -Dhttp.keepAlive=false \
     -ntp -pl system failsafe:integration-test
 
 sleep 10
-grep loadAverage frontend/target/liberty/wlp/usr/servers/defaultServer/logs/messages.log || exit 1
-grep memoryUsage frontend/target/liberty/wlp/usr/servers/defaultServer/logs/messages.log || exit 1
+grep loadAverage client/target/liberty/wlp/usr/servers/defaultServer/logs/messages.log || exit 1
+grep memoryUsage client/target/liberty/wlp/usr/servers/defaultServer/logs/messages.log || exit 1
 
 mvn -ntp -pl system liberty:stop
-mvn -ntp -pl frontend liberty:stop
+mvn -ntp -pl client liberty:stop
 
 mvn -ntp failsafe:verify
